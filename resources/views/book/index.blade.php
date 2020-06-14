0e22bcbdd994009ec344225af7c2ac859b26ea9c
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-14 15:20:58 +0900

    add template

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,7 +1,5 @@
-<head>
-  <title>Laravel Sample</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+@extends('book/layout')  //部分テンプレートを引っ張ってくる(継承)
+@section('content')    //この部分はよくわからん
 <div class="container ops-main">
 <div class="row">
   <div class="col-md-12">
@@ -39,3 +37,4 @@
     <div><a href="/book/create" class="btn btn-default">新規作成</a></div>
   </div>
 </div>
+@endsection
