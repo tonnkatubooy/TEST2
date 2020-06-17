d70575f7426e92f8ee77b836ac34d3120635dabb
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-17 16:22:29 +0900

    add action

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -22,4 +22,38 @@ class BookController extends Controller
       // 取得した値をビュー「book/edit」に渡す
         return view('book/edit', compact('book'));
     }
+    public function update(Request $request, $id)    //updateアクション
+    {
+        $book = Book::findOrFail($id);
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect("/book");
+    }
+    public function destroy($id)    //destroyアクション
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect("/book");
+    }
+    public function create()
+{
+    // 空の$bookを渡す
+    $book = new Book();
+        return view('book/create', compact('book'));
+    }
+    
+    public function store(Request $request)
+    {
+        $book = new Book();
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect("/book");
+    }
 }
